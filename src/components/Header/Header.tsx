7db6a1c7218ba32bd724f3981cbd932afae84e28
VD <52845106+Vhydie@users.noreply.github.com>
2020-10-19 20:23:40 +0700

    Update Header.tsx
    fix one

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,23 @@
-import React, { ReactElement } from 'react'
+import React, {ReactElement} from 'react'
 import styles from './Header.module.scss'
 
 const Header = () : ReactElement => {
   return (
-    <div className={styles.container}>
-      <div className={styles.title}>
-        UTS Teknologi Sistem Terintegrasi II3160
+    <div className = {styles.container}>
+      <div className = {styles.title}>
+        Ujian Tengah Semester
       </div>
-      <div className={styles.title}>
-        Tafia Alifianty Dinita Putri
+      <div className = {styles.title}>
+        II3160 Teknologi Sistem Terintegrasi
       </div>
-      <div className={styles.title}>
-        18218038
+      <div className = {styles.title}>
+        Simple Web Application using Azure
+      </div>
+      <div className = {styles.title}>
+        Vhydie Gilang Christianto The
+      </div>
+      <div className = {styles.title}>
+        18218016
       </div>
     </div>
   )
